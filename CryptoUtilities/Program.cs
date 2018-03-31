a331e803147b7af3f0272f731c977b39693db4ed
juansuerogit <juan.suero@gmail.com>
2018-03-31 12:50:38 -0400

    ...

---
+++ CryptoUtilities/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CryptoUtilities
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
